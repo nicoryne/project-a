3b3c5a9eeaf126c926c2abfa3f5189da2182c71b
Nicolo Porter <98087756+nicoryne@users.noreply.github.com>
2024-05-31 22:33:35 +0800

    [Nicolo] updated converter.ts
    Added binarization and temporarily adjusted view to see processed image results

--- src/lib/converter.ts
+++ src/lib/converter.ts
@@ -1,12 +1,23 @@
-import { createWorker } from 'tesseract.js';
-import cv from '@techstark/opencv-js';
+import { createWorker } from "tesseract.js";
+import { Image } from "image-js";
 
 const converter = async (img: string) => {
+  const rawImage = await Image.load(img);
+  const processedImage = rawImage
+  .grey()
+  .mask({
+    algorithm: 'threshold',
+    threshold: 0.6,
+  })
+  .invert();
+
+
+  const dataImage = processedImage.toDataURL();
   const worker = await createWorker("eng");
-  const ret = await worker.recognize(img);  
+  const ret = await worker.recognize(dataImage);
   const data = ret.data.text;
   await worker.terminate();
   return data;
-}
+};
 
 export default converter;
